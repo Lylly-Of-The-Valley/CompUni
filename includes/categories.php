55cccc14dcd55cb8e06d371c9f5ad14a28f5ae72
Traqn <traqnzlatanov77@gmail.com>
2014-08-21 17:26:48 +0300

    Add includes

---
+++ includes/categories.php
@@ -0,0 +1,21 @@
+<!--Categories-->
+<div class="col-md-3 col-xs-12 pull-right">
+    <div class="row">
+        <div class="categories">Categories</div>
+    </div>
+    <div class="row">
+        <ul>
+            <li>
+                <a href="#">Hardware</a>
+                <ul>
+                    <li><a href="#">GPU</a></li>
+                    <li><a href="#">CPU</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Software</a>
+            </li>
+        </ul>
+
+    </div>
+</div>
