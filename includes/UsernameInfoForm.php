6ebaba3cb04af1661eff7762e00f2187a165a73d
Daniel <petrovaliev95@abv.bg>
2014-08-26 21:42:58 +0300

    added user profile page

--- includes/UsernameInfoForm.php
+++ includes/UsernameInfoForm.php
@@ -1,52 +1,56 @@
-﻿<?php $information = mysql_query("SELECT * FROM users where user_username ='".$_SESSION['username_s']."'");
+<?php $information = mysql_query("SELECT * FROM users where user_username ='".$_SESSION['username_s']."'");
 $info= mysql_fetch_array($information, MYSQL_ASSOC);
 ?>
 
-<form method="post" class="form-horizontal" action="register.php?">
-    <fieldset>
-        <legend>User Information</legend>
-        <div class="form-group">
-		 <p>
-			<h4>
-				<?= 'First name: '.htmlentities($info['users_firstname']);?>
-			</h4>
-		</p>
-		<p>
-			<h4>
-				<?= 'Last name: '.htmlentities($info['users_lastname']);?>
-			</h4>
-		</p>
-		<p>
-			<h4>
-				<?= 'Gender: '.htmlentities($info['users_gender']);?>
-			</h4>
-		</p>
-		<p>
-			<h4>
-				<?= 'Town: '.htmlentities($info['user_town']);?>
-			</h4>
-		</p>
-		<p>
-		
-			<h4>
-				<?= 'User Name: '.htmlentities($info['user_username']);?>
-			</h4>
-		</p>
-		<p>
-			<h4>
-				<?= 'Email: '.htmlentities($info['users_email_address']);?>
-			</h4>
-		</p>
-		<p>
-			<h4>
-				<?= 'Description: '.htmlentities($info['user_description']);?>
-			</h4>
-		</p>
 
-					
-        </div>
-       
-    </fieldset>
-</form>
+<div class="modal-content user-profile-panel col-md-6">
+    <div class="modal-header">
+        <h4 class="modal-title">
+            <?= 'More About: '.htmlentities($info['users_username']);?>
+        </h4>
+    </div>
+    <div class="modal-body">
+        <center>
+            <img src="../images/defaultAvatar.png" width="140"  class="img-circle" border="0" height="140" alt="avatar"/>
 
+            <h3 class="media-heading">
+                <?= ''.htmlentities($info['users_firstname']) . htmlentities($info['users_lastname']);?>
+            </h3>
 
+            <span>
+                <strong>Gender:</strong>
+            </span>
+            <span class="label label-warning">
+                <?= ''.htmlentities($info['users_gender']);?>
+            </span>
+            <br/>
+            <span>
+                <strong>
+                    Town:
+                </strong>
+            </span>
+            <span class="label label-info">
+                <?= ''.htmlentities($info['user_town']);?>
+            </span>
+            <br/>
+            <span>
+                <strong>Email:</strong>
+            </span>
+
+            <span class="label label-success">
+                <?= 'Email: '.htmlentities($info['users_email_address']);?>
+            </span>
+
+            <hr/>
+
+            <center>
+                <p class="text-left">
+                    <strong>Description:</strong>
+                    <br/>
+                    <?= ''.htmlentities($info['user_description']);?>
+                </p>
+            </center>
+
+        </center>
+    </div>
+</div>
