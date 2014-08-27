342208e86419ebd02361c96cb50b6349f8811d75
Okiana <oki4102@abv.bg>
2014-08-27 15:26:05 +0300

    remove all

--- HomePageTemplate.php
+++
@@ -1,5 +0,0 @@
-<?php
-require('includes/headerForm.php');
-require('includes/categories.php');
-require('includes/topicsForm.php');
-require('includes/footerForm.php');
--- UsernameInfo.php
+++
@@ -1,6 +0,0 @@
-<?php
-require ("includes/connect.php");
-require("includes/headerForm.php");
-require("includes/UsernameInfoForm.php");
-require("includes/footerForm.php");
-?>
